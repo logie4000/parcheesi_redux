201ea6fd77ea968681039b0924cd4410b0c67ffd
Robert Mathews <logie4000@gmail.com>
2026-06-29 13:49:37 -0400

    Add JsonWebToken support

---
+++ api/app/lib/json_web_token.rb
@@ -0,0 +1,22 @@
+class JsonWebToken
+  # secret to encode and decode the token
+  HMAC_SECRET = Rails.application.secrets.secret_key_base
+
+  def self.encode(payload, exp = 24.hours.from_now)
+    payload[:exp] = exp.to_i
+ 
+    # sign the token with the application secret
+    JWT.encode(payload, HMAC_SECRET)
+  end
+
+  def self.decode(token)
+    begin
+      # get payload; first index in decoded library
+      body = JWT.decode(token, HMAC_SECRET)[0]
+      HashWithIndifferentAccess.new body
+
+    rescue JWT::DecodeError => e
+      raise ExceptionHandler::InvalidToken, e.message
+    end
+  end
+end
