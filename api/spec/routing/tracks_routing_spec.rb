a98a8440fdbbf8a99f3acaafdb4d62f14b3ee77e
Robert Mathews <logie4000@gmail.com>
2026-06-29 12:42:17 -0400

    Update tracks routing tests

--- api/spec/routing/tracks_routing_spec.rb
+++ api/spec/routing/tracks_routing_spec.rb
@@ -10,21 +10,20 @@ RSpec.describe Api::V1::TracksController, type: :routing do
       expect(get: "/api/v1/tracks/1").to route_to("api/v1/tracks#show", id: "1")
     end
 
-
     it "routes to #create" do
-      expect(post: "/api/v1/tracks").to route_to("api/v1/tracks#create")
+      expect(post: "/api/v1/tracks").not_to be_routable
     end
 
     it "routes to #update via PUT" do
-      expect(put: "/api/v1/tracks/1").to route_to("api/v1/tracks#update", id: "1")
+      expect(put: "/api/v1/tracks/1").not_to be_routable
     end
 
     it "routes to #update via PATCH" do
-      expect(patch: "/api/v1/tracks/1").to route_to("api/v1/tracks#update", id: "1")
+      expect(patch: "/api/v1/tracks/1").not_to be_routable
     end
 
     it "routes to #destroy" do
-      expect(delete: "/api/v1/tracks/1").to route_to("api/v1/tracks#destroy", id: "1")
+      expect(delete: "/api/v1/tracks/1").not_to be_routable
     end
   end
 end
