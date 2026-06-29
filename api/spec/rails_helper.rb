d2d040a30001cf71ece27f7dbedd549e27d92eaa
Robert Mathews <logie4000@gmail.com>
2026-06-29 12:55:09 -0400

    Maybe database cleaner is unnecessary

--- api/spec/rails_helper.rb
+++ api/spec/rails_helper.rb
@@ -71,7 +71,7 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   # require database cleaner at the top level
-  require 'database_cleaner'
+  # require 'database_cleaner'
 
   # [...]
   # configure shoulda matchers to use rspec as the test framework and full matcher libraries for rails
@@ -91,17 +91,17 @@ RSpec.configure do |config|
     config.include ControllerSpecHelper
 
     # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
-    config.before(:suite) do
-      DatabaseCleaner.clean_with(:truncation)
-      DatabaseCleaner.strategy = :transaction
-    end
+    # config.before(:suite) do
+    #   DatabaseCleaner.clean_with(:truncation)
+    #   DatabaseCleaner.strategy = :transaction
+    # end
 
-    # start the transaction strategy as examples are run
-    config.around(:each) do |example|
-      DatabaseCleaner.cleaning do
-        example.run
-      end
-    end
+    # # start the transaction strategy as examples are run
+    # config.around(:each) do |example|
+    #   DatabaseCleaner.cleaning do
+    #     example.run
+    #   end
+    # end
     # [...]
   end
 end
