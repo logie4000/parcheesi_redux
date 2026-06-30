21a8001e16c5a68ecca9777b97aede061878b272
Robert Mathews <logie4000@gmail.com>
2026-06-30 13:18:55 -0400

    Fix song service endpoint

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -43,7 +43,7 @@ export const routes: Routes = [
         path: 'radioshows/:id',
         component: RadioShowComponent
     }, 
-        {
+    {
         path: 'songs',
         component: SongIndexComponent
     },
